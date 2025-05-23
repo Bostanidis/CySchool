0893353c8fc561a72f9e9ec35efff9056879730c
Alexandros Bostanidis <alexandrosbostanidis09@gmail.com>
2025-05-23 09:15:31 +0300

    Added database

---
+++ server/db.js
@@ -0,0 +1,12 @@
+// db.js
+const { Pool } = require("pg")
+
+const pool = new Pool({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT, // default PostgreSQL port
+});
+
+module.exports = pool;
